3f84b0309d9e20715934bbaae272b1945949b005
ritchieanesco <info@ritchieanesco.com>
2018-10-13 22:52:29 +1100

    -  Add global styles - Setup global nav links

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,34 @@
 import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
 
 const Nav = () => (
-  <div>
-    <Link href="/sell">
-      <a>Go to Sell!</a>
-    </Link>
-    <Link href="/">
-      <a>Go to Home</a>
-    </Link>
-  </div>
+  <NavStyles>
+    <li>
+      <Link href="/items">
+        <a>Items</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/signup">
+        <a>Signup</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/me">
+        <a>Account</a>
+      </Link>
+    </li>
+  </NavStyles>
 );
 
 export default Nav;
